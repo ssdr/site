1cbf8f7abe4cef0ff2725fe163365de801142733
liuyan <yanke.ly@alibaba-inc.com>
2025-03-15 17:26:38 +0800

    update photos.yml

--- .github/workflows/scripts/images.js
+++ .github/workflows/scripts/images.js
@@ -2,13 +2,13 @@ import fs from 'fs'
 import path from 'path'
 
 const datetime = process.argv[2]
-const fileName = process.argv[3]
+const filename = process.argv[3]
 const content = process.argv[4]
 
 const alphabets = [...'umoacenrvwz']
-const idx = fs.readdirSync('_notes').filter(p => p.startsWith(datetime.split(' ')[0])).length
+const idx = fs.readdirSync('images').filter(p => p.startsWith(datetime.split(' ')[0])).length
 const a = alphabets[idx]
-const filePath = `images/${datetime.split(' ')[0]}-${fileName}-${a}${a}.jpg`
+const filePath = `images/${datetime.split(' ')[0]}-${filename}-${a}${a}.jpg`
 
 const img = Buffer.from(content, 'base64').toString('binary')
 
