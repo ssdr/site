1cd81923c8581c80d481cb605909ff333f158eaf
liuyan <yanke.ly@alibaba-inc.com>
2025-03-15 16:27:26 +0800

    update photos.yml

--- .github/workflows/scripts/images.js
+++ .github/workflows/scripts/images.js
@@ -1,36 +1,15 @@
 import fs from 'fs'
 import path from 'path'
 
-const filePath = process.argv[2]
-const jsonString = process.argv[3]
-const fileExists = fs.existsSync(filePath)
+const datetime = process.argv[2]
+const fileName = process.argv[3]
+const content = process.argv[4]
 
-const photos = fileExists ? JSON.parse(fs.readFileSync(filePath).toString()) : []
-const json = JSON.parse(jsonString)
-photos.push(json)
+const alphabets = [...'umoacenrvwz']
+const idx = fs.readdirSync('_notes').filter(p => p.startsWith(datetime.split(' ')[0])).length
+const a = alphabets[idx]
+const filePath = `images/${datetime.split(' ')[0]}-${fileName}-${a}${a}.jpg`
 
-const dirName = path.dirname(filePath)
-if (!fileExists && dirName !== '.') {
-  fs.mkdirSync(dirName, {recursive: true})
-}
+const img = Buffer.from(content, 'base64').toString('binary')
 
-const date = new Date(json.uploaded)
-const content = `---
-layout: story
-date: ${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}
-tags: [ ${json.meta.tags.join(', ')} ]
-title: Story
-image: https://photos.muan.dev/cdn-cgi/imagedelivery/-wp_VgtWlgmh1JURQ8t1mg/${json.id}/public
-caption: |
-  ${json.meta.caption}
-alt: |
-  ${json.meta.alt}
----
-
-![${json.meta.alt}](https://photos.muan.dev/cdn-cgi/imagedelivery/-wp_VgtWlgmh1JURQ8t1mg/${json.id}/public)
-
-${json.meta.caption}
-`
-
-fs.writeFileSync(`_stories/${json.id}.md`, content)
-fs.writeFileSync(filePath, JSON.stringify(photos, null, 2))
+fs.writeFileSync(filePath, img)
